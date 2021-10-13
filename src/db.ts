835b669d9d05838537de6663fd4e5f323446c2dc
Bruno Domingues <77869200+brunood07@users.noreply.github.com>
2021-10-13 13:13:04 -0300

    Update db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from 'pg';
 
-const connectionString = 'postgres://lfmpgdsb:sBolDd_I6Zy_aVutJGdwmQPj3o40RwBm@motty.db.elephantsql.com/lfmpgdsb';
+const connectionString = '';
 
 const db = new Pool({ connectionString });
 
